8fe2cc5ef1f4b239eed8f9a6e550d945c119b706
= <=>
2024-10-11 08:40:01 -0300

    uncommented view seving

--- src/app.ts
+++ src/app.ts
@@ -75,10 +75,10 @@ class AllInOneServer extends BasicServer {
     this.withCookies();
     this.withcors('http://localhost:' + process.env.FRONTEND_PORT);
     this.router(allRouter);
-    // this.server.use(express.static(path.resolve('./front')));
-    // this.server.use('*', (req,res) => {
-    //   res.sendFile(path.resolve('./front/index.html'))
-    //   });
+    this.server.use(express.static(path.resolve('./front')));
+    this.server.use('*', (req,res) => {
+      res.sendFile(path.resolve('./front/index.html'))
+      });
   }
 }
 

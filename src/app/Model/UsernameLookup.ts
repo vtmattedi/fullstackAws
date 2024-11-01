73eb0af388e7c20b20450434ccda9d8b19dd662b
vtmattedi <vtmattedi@gmail.com>
2024-11-01 16:21:13 -0300

    fixed user auto increment

--- src/app/Model/UsernameLookup.ts
+++ src/app/Model/UsernameLookup.ts
@@ -4,7 +4,7 @@ export class UsernameLookup {
     /**
     * A map to store usernames with their corresponding user IDs.
     */
-    static usernameMap = new Map<Number, String>();
+    static usernameMap = new Map<number, String>();
     /**
     * Loads usernames from the database and populates the usernameMap.
     * 
@@ -17,6 +17,7 @@ export class UsernameLookup {
             for (const user of users) {
                 UsernameLookup.addUsername(user.id, user.username);
             }
+            UsernameLookup.#setMaxId();
             console.log("Loaded usernames from database");
         }
         catch (err) {
@@ -27,29 +28,33 @@ export class UsernameLookup {
     /**
     * Adds a username to the usernameMap.
     * 
-    * @param {Number} uid - The user ID.
+    * @param {number} uid - The user ID.
     * @param {String} username - The username.
     */
-    static addUsername(uid: Number, username: String) {
+    static addUsername(uid: number, username: String) {
         UsernameLookup.usernameMap.set(uid, username);
+        if (uid > UsernameLookup.MAXID) {
+            UsernameLookup.MAXID = uid;
+        }
     }
     
     /**
      * Deletes a username from the usernameMap.
      * 
-     * @param {Number} uid - The user ID.
+     * @param {number} uid - The user ID.
      */
-    static deleteUsername(uid: Number) {
+    static deleteUsername(uid: number) {
         UsernameLookup.usernameMap.delete(uid);
+    UsernameLookup.#setMaxId();
     }
 
     /**
     * Retrieves a username from the usernameMap.
     * 
-    * @param {Number} uid - The user ID.
+    * @param {number} uid - The user ID.
     * @returns {String | undefined} The username or "User deleted" if the user ID does not exist.
     */
-    static getUsername(uid: Number) {
+    static getUsername(uid: number) {
         if (!UsernameLookup.usernameMap.has(uid)) {
             return "User deleted";
         }
@@ -60,10 +65,28 @@ export class UsernameLookup {
     /**
      * Edits a username in the usernameMap.
      * 
-     * @param {Number} uid - The user ID.
+     * @param {number} uid - The user ID.
      * @param {String} username - The new username.
      */
-    static editUsername(uid: Number, username: String) {
+    static editUsername(uid: number, username: String) {
         UsernameLookup.usernameMap.set(uid, username);
     }
+
+    
+    /**
+     * A private static method that sets the maximum ID (`MAXID`) in the `UsernameLookup` class.
+     * It iterates through the keys of the `usernameMap` and updates `MAXID` to the highest key value found.
+     * This method is used to ensure that `MAXID` always holds the highest key value present in the map.
+     */
+    static #setMaxId(){
+        UsernameLookup.MAXID = 0;
+        for (let key of UsernameLookup.usernameMap.keys()) {
+            if (key > UsernameLookup.MAXID) {
+                UsernameLookup.MAXID = key;
+            }
+        }
+    }
+
+    // Static variable to store the maximum ID of the users table (For auto-incrementing user IDs in Postgress)
+    static MAXID: number = 0;
 }
